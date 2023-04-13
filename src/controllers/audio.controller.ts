078876c3f7155c2aa4a9814da691ae165a407cf6
codestronomer <johnrumide6@gmail.com>
2023-04-13 10:47:17 +0100

    [fix] xmljs2 security vunerability

--- src/controllers/audio.controller.ts
+++ src/controllers/audio.controller.ts
@@ -66,8 +66,8 @@ export const getBeatsById = async (req: Request, res: Response) => {
 
 
 export const getBeats = async (req: Request, res: Response) => {
-        const page: number = parseInt(req.query?.page);
-        const limit: number = parseInt(req.query?.limit);
+        const page: number = req.query?.page;
+        const limit: number = req.query?.limit;
         try {
                 // execute query with page and limit values
                 const beats = await BeatModel.find()
