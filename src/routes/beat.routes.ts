fd1a68a23eddf8af7b78adbc0f18dc834837ac89
Teepee78 <eniolatoluwalase78@gmail.com>
2023-04-16 20:42:46 +0100

    Add user id in request body from authentication middleware

--- src/routes/beat.routes.ts
+++ src/routes/beat.routes.ts
@@ -1,21 +1,24 @@
-import { Router} from "express";
-import auth from '../middlewares/auth'
-import { uploadBeat, getBeatsById, deleteBeat, getBeats } from '../controllers/beat.controller';
-import upload from '../middlewares/upload.middleware';
-
-
-/**
- * Initialize express router 
-*/
-const router: Router = Router()
-
+import { Router } from "express";
+import auth from "../middlewares/auth";
+import {
+	uploadBeat,
+	getBeatsById,
+	deleteBeat,
+	getBeats,
+} from "../controllers/beat.controller";
+import upload from "../middlewares/upload.middleware";
 
 /**
-* Define audio routes
-*/
-router.post('', auth, upload.single('audio'), uploadBeat);
-router.get('/:id', auth, getBeatsById);
-router.get('', getBeats);
-router.delete('', auth, deleteBeat);
+ * Initialize express router
+ */
+const router: Router = Router();
+
+/**
+ * Define audio routes
+ */
+router.post("", [upload.single("audio"), auth], uploadBeat);
+router.get("/:id", auth, getBeatsById);
+router.get("", getBeats);
+router.delete("", auth, deleteBeat);
 
 export default router;
