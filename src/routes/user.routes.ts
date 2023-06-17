14cfeb9c6f27dfe7417e2686251e55d26b3e4c49
codestronomer <johnrumide6@gmail.com>
2023-06-18 00:50:04 +0100

    [feat] add reviewController to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller";
+import * as reviewController from "../controllers/reviews.controller";
 import auth from "../middlewares/auth";
 import upload from "../middlewares/upload.middleware";
 
@@ -12,6 +13,7 @@ router.put("/update", auth, userController.update);
 router.post("/confirmUser", userController.confirmUsername);
 router.post("/confirmEmail", userController.confirmEmail);
 router.post("/", userController.getUserWithName);
+router.post("/review", auth, reviewController.createReview);
 router.get("/trendingProducers", userController.getTrendingProducers);
 router.get("/trendingEngineers", userController.getTrendingSoundEngineers);
 router.get("/profile", auth, userController.getProfileImage);
